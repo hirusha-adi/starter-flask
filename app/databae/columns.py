59db2805878be7f0b450465cebd3e148e6b2baa7
hirusha-adi <hirushaadi@gmail.com>
2024-08-08 22:14:38 +0530

    Create columns.py

---
+++ app/databae/columns.py
@@ -0,0 +1,14 @@
+COLUMNS_users = (
+    "user_id",
+    "first_name",
+    "last_name",
+    "email",
+    "password",
+    "is_admin",
+    "is_super_admin",
+)
+
+COLUMNS_contactus = (
+    "key",
+    "value",
+)
